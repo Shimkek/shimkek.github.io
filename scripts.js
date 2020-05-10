4dcd149eb4aa1c158bc3f9d34d2c16547fe74b73
shimkek <maksim.dunaitsev@gmail.com>
2020-05-11 01:55:55 +0600

    yes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
-let createdTasks = 0;
+const stringToArray = (str) => {
+    var arr = [];
+    for(i=0; i<=str.length; i++)
+        arr[i] = str.charAt(i);
+    return arr;
+}
+
+const arrayToString = (arr) => {
+    var str = "";
+    for(i=0; i<=arr.length; i++)
+    str +=arr.charAt(i);
+    return str;
+}
+
+const fillTable = () => {
+}
+
+
+const buttonScript = () => {
+    window.localStorage.clear();
+    window.localStorage.setItem('createdTasks', 0);
+    window.localStorage.removeItem('tasks');
+}
 
 
 const twoNumberDate = (date) => {
     const string = new String(date);
     if (string.length === 1) {
-        return '0'+date;
+        return '0' + date;
     }
     else {
         return date;
@@ -12,33 +34,33 @@ const twoNumberDate = (date) => {
 }
 
 const appendTextnode = (id, node) => {
-    let textnode = document.createTextNode(node);   
+    let textnode = document.createTextNode(node);
     document.getElementById(id).appendChild(textnode);
 }
 const convertMonthToName = (month) => {
     if (month === 1) {
         return "January";
-    }else if (month === 2) {
+    } else if (month === 2) {
         return "February";
-    }else if (month === 3) {
+    } else if (month === 3) {
         return "March";
-    }else if (month === 4) {
+    } else if (month === 4) {
         return "April";
-    }else if (month === 5) {
+    } else if (month === 5) {
         return "May";
-    }else if (month === 6) {
+    } else if (month === 6) {
         return "June";
-    }else if (month === 7) {
+    } else if (month === 7) {
         return "July";
-    }else if (month === 8) {
+    } else if (month === 8) {
         return "August";
-    }else if (month === 9) {
+    } else if (month === 9) {
         return "September";
-    }else if (month === 10) {
+    } else if (month === 10) {
         return "October";
-    }else if (month === 11) {
+    } else if (month === 11) {
         return "November";
-    }else if (month === 12) {
+    } else if (month === 12) {
         return "December";
     } else {
         return "error1"
@@ -47,47 +69,85 @@ const convertMonthToName = (month) => {
 
 const timeNow = () => {
     const today = new Date();
-    const date = twoNumberDate(today.getDate())+'-'+convertMonthToName((today.getMonth()+1))+'-'+today.getFullYear();
+    const date = twoNumberDate(today.getDate()) + '-' + convertMonthToName((today.getMonth() + 1)) + '-' + today.getFullYear();
     const time = twoNumberDate(today.getHours()) + ":" + twoNumberDate(today.getMinutes()) + ":" + twoNumberDate(today.getSeconds());
-    const dateTime = time+' '+date;
+    const dateTime = time + ' ' + date;
 
     return dateTime;
 
-}   
+}
 
-const deleteTask = (id) => { 
-    document.getElementById(id).remove(); 
+const deleteTask = (id) => {
+ //   if (window.confirm("Are you sure?") === true) {
+        document.getElementById(id).remove();
+ //   }
 }
 
 const addTask = () => {
-    const inputBox = document.getElementById("inputBox"); 
-    const taskText = document.createTextNode(inputBox.value);
-    const row = document.createElement("tr");
+    if (inputBox.value !== "") {
 
-    const newStatus = document.createElement('td');
-    const newTask = document.createElement("td");
-    const newTimeStamp = document.createElement("td");
-    const newDeleteButton = document.createElement("td");
+        const inputBox = document.getElementById("inputBox");
+        const taskText = document.createTextNode(inputBox.value);
+        const row = document.createElement("tr");
 
-    const statusValue = document.createTextNode('Not Completed');
-    newStatus.appendChild(statusValue);
+        const newStatus = document.createElement('td');
+        const newTask = document.createElement("td");
+        const newTimeStamp = document.createElement("td");
+        const newDeleteButton = document.createElement("td");
 
-    newTask.appendChild(taskText);
+        const statusValue = document.createTextNode('Not Completed');
+        newStatus.appendChild(statusValue);
 
-    const timeStampValue = document.createTextNode(timeNow());
-    newTimeStamp.appendChild(timeStampValue);
+        newTask.appendChild(taskText);
 
+        const timeStampValue = document.createTextNode(timeNow());
+        newTimeStamp.appendChild(timeStampValue);
 
-   //const codeBlock ='<button class="button is-danger is-outlined is-small"><span class="icon"></span><i class="fas fa-times"></i></button>';
-   // const deleteButtonValue = document.createTextNode('');
-   // deleteButtonValue.innerHTML = codeBlock;
-   // newDeleteButton.appendChild(deleteButtonValue);
+        
 
-    row.appendChild(newStatus);
-    row.appendChild(newTask);
-    row.appendChild(newTimeStamp);
-    row.appendChild(newDeleteButton);
+        row.appendChild(newStatus);
+        row.appendChild(newTask);
+        row.appendChild(newTimeStamp);
+        row.appendChild(newDeleteButton);
 
-    document.getElementById("todotablebody").appendChild(row);
-    inputBox.value = "";
+        document.getElementById("todotablebody").appendChild(row);
+        
+        inputBox.value = "";
+        
+        
+        //
+        
+        
+        var text = window.localStorage.getItem('createdTasks');
+        var integer = parseInt(text, 10);
+        integer++;
+        window.localStorage.setItem('createdTasks', integer);
+    
+
+        if( tasks !== null) {
+        var tasks = stringToArray(window.localStorage.getItem('tasks'));
+        };
+        
+        appendTextnode('abc', tasks);
+        
+        
+        const task = {
+            'taskNumber' :  integer,
+            'taskStatus' : 'Not Completed',
+            'taskText' : inputBox.value,
+            'taskTime' : timeNow()
+        }
+        
+       // tasks.push(task);
+      //  appendTextnode('\nabc', tasks);
+        
+        window.localStorage.setItem('tasks', tasks);
+       // tasks.push(task);
+
+       // window.localStorage.getItem('tasks')
+    
+       // window.alert(tasks);
+
+        
+    }
 }
